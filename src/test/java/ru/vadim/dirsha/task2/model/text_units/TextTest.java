603c9b42bcbce26187019a019866d2c890fa8769
vadim-dirsha <faqproshival@yandex.ru>
2018-11-19 10:55:19 +0300

    update test data

--- src/test/java/ru/vadim/dirsha/task2/model/text_units/TextTest.java
+++ src/test/java/ru/vadim/dirsha/task2/model/text_units/TextTest.java
@@ -2,11 +2,25 @@ package ru.vadim.dirsha.task2.model.text_units;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import ru.vadim.dirsha.task2.model.text_units.abstract_units.ITextUnit;
+import ru.vadim.dirsha.task2.model.text_units.abstract_units.SubTextUnit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.testng.Assert.*;
+import static ru.vadim.dirsha.task2.utils.FileLLJava.readAllFile;
 
 public class TextTest {
+
+    public static final String DELIMITER = "\r\n";
+    private static String TEST_DATA = "/sentence/testdata";
+    private static String TEST_RESULT = "/sentence/testresult";
 
     @BeforeMethod
     public void setUp() {
@@ -16,7 +30,79 @@ public class TextTest {
     public void tearDown() {
     }
 
-    @Test
-    public void testParseDataToTextUnitCollection() {
+
+    @DataProvider(name = "data")
+    public Object[][] data() {
+
+        ArrayList<ArrayList<Object>> result = new ArrayList<>();
+
+        for (int i = 1; this.getClass().getResource(TEST_DATA + i) != null && this.getClass().getResource(TEST_RESULT + i) != null; i++) {
+            ArrayList<Object> temp = new ArrayList<>();
+
+            try {
+                temp.add(readAllFile(getClass().getResourceAsStream(TEST_DATA + i)));
+
+                String str = readAllFile(getClass().getResourceAsStream(TEST_RESULT + i));
+                BufferedReader bufferedReader = new BufferedReader(new StringReader(str));
+
+                int size = str.replaceAll("[^" + DELIMITER + "]", "").length() / DELIMITER.length();
+                String[] strtemp = str.split(DELIMITER);
+
+                List<ITextUnit> tempList = new ArrayList<>();
+                String leftSide = strtemp[0];
+
+                String rightSide = "";
+                if (size == strtemp.length) {
+                    for (int j = 1; j < strtemp.length; j++) {
+                        tempList.add(new Word(strtemp[j]));
+                    }
+
+                } else {
+                    for (int j = 1; j < strtemp.length - 1; j++) {
+                        tempList.add(new Word(strtemp[j]));
+                    }
+                    rightSide = strtemp[strtemp.length - 1];
+                }
+
+                temp.add(new SubTextUnit<List<ITextUnit>>(tempList, leftSide, rightSide));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            result.add(temp);
+        }
+
+        Object[][] object = new Object[result.size()][2];
+        for (int i = 0; i < result.size(); i++) {
+            object[i] = result.get(i).toArray();
+        }
+        return object;
+    }
+
+
+    @Test(dataProvider = "data")
+    public void testLeftSide(String data, SubTextUnit<List<ITextUnit>> result) {
+        Sentence sentence = new Sentence(data);
+        assertEquals(sentence.getLeftSide(), result.getLeftSide());
+    }
+
+    @Test(dataProvider = "data", dependsOnMethods = "testLeftSide")
+    public void testRightSide(String data, SubTextUnit<List<ITextUnit>> result) {
+        Sentence sentence = new Sentence(data);
+        assertEquals(sentence.getRightSide(), result.getRightSide());
+    }
+
+    @Test(dataProvider = "data", dependsOnMethods = "testRightSide")
+    public void testSize(String data, SubTextUnit<List<ITextUnit>> result) {
+        Sentence sentence = new Sentence(data);
+        assertEquals(result.getValue().size(), sentence.size());
+    }
+
+    @Test(dataProvider = "data", dependsOnMethods = "testSize")
+    public void testParseDataToTextUnitCollection(String data, SubTextUnit<List<ITextUnit>> result) {
+        Sentence sentence = new Sentence(data);
+        for (int i = 0; i < result.getValue().size(); i++) {
+            assertEquals(sentence.get(i).toText(), result.getValue().get(i).toText());
+        }
     }
 }
